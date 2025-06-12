8108fec69985b33aba8a19f815f760aa5fd1790b
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-06-12 01:25:50 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
-LOCATION='lib/'
+# Initalize "global" variables
+PERM_DIR='lib'
+TEMP_DIR='temp-build-script-dir'
 REPO='https://github.com/payne1778/Translation-Library-Docs'
 COMMIT='main'
 
 # Adds the subtree of the Translation Library Docs repository
-git subtree add --prefix=temp-build-script-dir "$REPO" "$COMMIT" --squash
+git subtree add --prefix="$TEMP_DIR/" "$REPO" "$COMMIT" --squash
+
+# Move files from the temp dir into the permenant dir location.
+# If the permenant dir location doesn't exist, create it before adding files
+if [ -d $PERM_DIR ]; then
+    mv "$TEMP_DIR/*" "$PERM_DIR/"
+else
+    mkdir "$PERM_DIR/"
+    mv "$TEMP_DIR/*" "$PERM_DIR/"
+fi
 
 # Removes the Translation Library Docs Repository's LICENSE and README.md
 # These files are either redundant or not needed in the prefix location
-rm lib/LICENSE
-rm lib/README.md
+if [ -e "$PERM_DIR/LICENSE" ] && [ -e "$PERM_DIR/README.md" ]; then
+    rm "$PERM_DIR/LICENSE"
+    rm "$PERM_DIR/README.md"
+fi
