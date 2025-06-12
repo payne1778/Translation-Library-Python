9101198e02e37b96cc2878b56491128aeafa19b6
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-06-12 01:13:03 -0500

    Update build.sh to fix issue

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 LOCATION='lib/'
-PREFIX='--prefix=$LOCATION/'
 REPO='https://github.com/payne1778/Translation-Library-Docs'
 COMMIT='main'
 
 # Adds the subtree of the Translation Library Docs repository
-git subtree add $PREFIX "$REPO" "$COMMIT" --squash
+git subtree add --prefix=$LOCATION "$REPO" "$COMMIT" --squash
 
 # Removes the Translation Library Docs Repository's LICENSE and README.md
 # These files are either redundant or not needed in the prefix location
