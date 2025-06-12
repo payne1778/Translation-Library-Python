c32dbc8de6c7465323cd0bdac10a257f9c501f63
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-06-12 01:32:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,13 @@ git subtree add --prefix="$TEMP_DIR/" "$REPO" "$COMMIT" --squash
 # Move files from the temp dir into the permenant dir location.
 # If the permenant dir location doesn't exist, create it before adding files
 if [ -d $PERM_DIR ]; then
-    mv "$TEMP_DIR/*" "$PERM_DIR/"
+    mv $TEMP_DIR/* "$PERM_DIR/"
 else
     mkdir "$PERM_DIR/"
-    mv "$TEMP_DIR/*" "$PERM_DIR/"
+    mv $TEMP_DIR/* "$PERM_DIR/"
 fi
+
+rm -rf "$TEMP_DIR"
 
 # Removes the Translation Library Docs Repository's LICENSE and README.md
 # These files are either redundant or not needed in the prefix location
