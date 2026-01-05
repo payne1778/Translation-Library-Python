01ecddb602abf5d4b004ca4e3aff0d30a25f0372
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-04 22:28:36 -0600

    set up venv in build script

--- build.sh
+++ build.sh
@@ -7,13 +7,20 @@ DOCS_REPO_NAME="Translation-Library-Docs"
 DOCS_REPO_URL="https://github.com/payne1778/$DOCS_REPO_NAME"
 COMMIT="main"
 
-# Puts contents of the docs repo into a new folder named with $DOCS_REPO_NAME
+# 1. Set up virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+# 2. Install dependencies
+pip install unittest
+
+# 3. Put contents of the docs repo into a new folder named with $DOCS_REPO_NAME
 git subtree add --prefix="$DOCS_REPO_NAME/" "$DOCS_REPO_URL" "$COMMIT" --squash
 
-# If the $LIB_DIR doesn't exist, create it before adding files to it
+# 4. If the $LIB_DIR doesn't exist, create it before adding files to it
 if [ ! -d $LIB_DIR ]; then
-    mkdir "$LIB_DIR/"    
+    mkdir "$LIB_DIR"    
 fi
 
-# Move files from the docs repo stored in $DOCS_REPO_NAME into $LIB_DIR
+# 5. Move files from the docs repo stored in $DOCS_REPO_NAME into $LIB_DIR
 mv "$DOCS_REPO_NAME" "$LIB_DIR"
