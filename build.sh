c4c0318e29041f96e04336d9c70ebfb1319a0ad0
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-04 21:54:46 -0600

    clarify names and simply script

--- build.sh
+++ build.sh
@@ -2,22 +2,20 @@
 
 set -e 
 
-# Initalize "global" variables
-PERM_DIR='lib'
-TEMP_DIR='temp-build-script-dir'
-REPO='https://github.com/payne1778/Translation-Library-Docs'
-COMMIT='main'
+LIB_DIR="lib"
+REPO_NAME="Translation-Library-Docs"
+REPO_URL="https://github.com/payne1778/$REPO_NAME"
+COMMIT="main"
 
-# Adds the subtree of the Translation Library Docs repository
-git subtree add --prefix="$TEMP_DIR/" "$REPO" "$COMMIT" --squash
+# Adds the subtree of the Translation Library Docs repository. 
+# Puts contents of the docs repo into a new folder named with $REPO_NAME
+git subtree add --prefix="$REPO_NAME/" "$REPO_URL" "$COMMIT" --squash
 
-# Move files from the temp dir into the permenant dir location.
-# If the permenant dir location doesn't exist, create it before adding files
-if [ -d $PERM_DIR ]; then
-    mv $TEMP_DIR/* "$PERM_DIR/"
+# Move files from the temp dir into the `lib` directory location.
+# If the `lib` dir location doesn't exist, create it before adding files to it
+if [ -d $LIB_DIR ]; then
+    mv "$REPO_NAME/" "$LIB_DIR/"
 else
-    mkdir "$PERM_DIR/"
-    mv $TEMP_DIR/* "$PERM_DIR/"
+    mkdir "$LIB_DIR/"
+    mv $REPO_NAME/* "$LIB_DIR/"
 fi
-
-rm -rf "$TEMP_DIR"
