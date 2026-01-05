9255d44ac3e6af2e062939d049b192858cc2db4c
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-04 22:04:09 -0600

    more clarity & script organization

--- build.sh
+++ build.sh
@@ -3,19 +3,17 @@
 set -e 
 
 LIB_DIR="lib"
-REPO_NAME="Translation-Library-Docs"
-REPO_URL="https://github.com/payne1778/$REPO_NAME"
+DOCS_REPO_NAME="Translation-Library-Docs"
+DOCS_REPO_URL="https://github.com/payne1778/$DOCS_REPO_NAME"
 COMMIT="main"
 
-# Adds the subtree of the Translation Library Docs repository. 
-# Puts contents of the docs repo into a new folder named with $REPO_NAME
-git subtree add --prefix="$REPO_NAME/" "$REPO_URL" "$COMMIT" --squash
+# Puts contents of the docs repo into a new folder named with $DOCS_REPO_NAME
+git subtree add --prefix="$DOCS_REPO_NAME/" "$DOCS_REPO_URL" "$COMMIT" --squash
 
-# Move files from the temp dir into the `lib` directory location.
-# If the `lib` dir location doesn't exist, create it before adding files to it
-if [ -d $LIB_DIR ]; then
-    mv "$REPO_NAME/" "$LIB_DIR/"
-else
-    mkdir "$LIB_DIR/"
-    mv $REPO_NAME/* "$LIB_DIR/"
+# If the $LIB_DIR doesn't exist, create it before adding files to it
+if [ ! -d $LIB_DIR ]; then
+    mkdir "$LIB_DIR/"    
 fi
+
+# Move files from the docs repo stored in $DOCS_REPO_NAME into $LIB_DIR
+mv "$DOCS_REPO_NAME" "$LIB_DIR"
