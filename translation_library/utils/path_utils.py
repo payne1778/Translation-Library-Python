90416d6aa16343d5f024cf3db1d0c6947c80c60e
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 17:30:27 -0600

    add raise_for_nonexistance() & docstrings for functions

--- translation_library/utils/path_utils.py
+++ translation_library/utils/path_utils.py
@@ -1,24 +1,62 @@
 from os import walk
-from os.path import join
+from os.path import exists
 from pathlib import Path
 from typing import Iterator, List, Tuple
 
 type Walker = Iterator[Tuple[str, List[str], List[str]]]
 
 
-def get_project_root(anchor_file=".git") -> Path:
+def raise_for_nonexistance(file_path: str | Path) -> None:
+    """
+    Raises `FileNotFoundError` if the path from arg `file_path` does not exist
+
+    Args:
+        file_path (str | Path): the path of whose existance to check
+
+    Raises:
+        FileNotFoundError: if the path could not be located or does not exist
+    """
+    if not exists(file_path):
+        raise FileNotFoundError(f"'{file_path}' could not be located/does not exist")
+
+
+def get_project_root(anchor=".git") -> Path:
+    """
+    Find and return the path of the root path of the project.
+
+    Args:
+        anchor (str, optional): a known file/dir that exists in the project root. Defaults to ".git".
+
+    Raises:
+        FileNotFoundError: if `anchor` could not be found in parent dirs
+
+    Returns:
+        Path: the path of the project root
+    """
     current_path = Path(__file__).resolve()
     for parent in current_path.parents:
-        if (parent / anchor_file).exists():
+        if (parent / anchor).exists():
             return parent
     raise FileNotFoundError(
-        f"Could not find '{anchor_file}' in the parent dirs of '{current_path}'"
+        f"Could not find '{anchor}' in the parent dirs of '{current_path}'"
     )
 
 
 def get_language_file_path(language: str) -> Path:
-    walker: Walker = walk(join(get_project_root() / "lib"))
-    for dir_path, dir_names, file_names in walker:
+    """
+    Get the path of the TOML file for a specified language name.
+
+    Args:
+        language (str): the name of language whose TOML file path to retrieve
+
+    Raises:
+        FileNotFoundError: if the language's TOML file could not be found (ex. `english.toml`)
+
+    Returns:
+        Path: the absoulte path of the specified language's TOML file
+    """
+    walker: Walker = walk(get_project_root() / "lib")
+    for dir_path, _, file_names in walker:
         for file_name in file_names:
             if file_name == f"{language.lower()}.toml":
                 return Path(dir_path) / Path(file_name)
@@ -27,7 +65,18 @@ def get_language_file_path(language: str) -> Path:
 
 
 def get_languages_file_path() -> Path:
+    """
+    Get the path of the TOML file that contains all supported languages.
+
+    Raises:
+        RuntimeError: if the languages TOML file, `languages.toml`, does not exist
+
+    Returns:
+        Path: the absoulte path to `languages.toml`
+    """
     try:
+        # Uses get_language_file_path() to avoid code duplication.
+        # Instead of `english.toml` or `german.toml`, it searches for `languages.toml`
         return get_language_file_path("languages")
     except FileNotFoundError:
-        raise RuntimeError("Languages TOML file does not exist")
+        raise FileNotFoundError("Could not find `languages.toml`")
