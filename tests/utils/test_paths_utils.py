8f589ed8d456cbab386988d703871dfc4c0c72b5
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 17:30:37 -0600

    test raise_for_nonexistance()

--- tests/utils/test_paths_utils.py
+++ tests/utils/test_paths_utils.py
@@ -1,10 +1,26 @@
+from pathlib import Path
+
 import pytest
 
 from translation_library.utils.path_utils import (
     get_language_file_path,
     get_languages_file_path,
     get_project_root,
+    raise_for_nonexistance,
 )
+
+PARENT_DIR_PATH = Path(__file__).resolve().parent
+
+EXAMPLE_ENGLISH_TOML_PATH: Path = PARENT_DIR_PATH / "example_english.toml"
+
+
+def test_raise_for_nonexistance() -> None:
+    raise_for_nonexistance(EXAMPLE_ENGLISH_TOML_PATH)
+
+
+def test_raise_for_nonexistance_fail() -> None:
+    with pytest.raises(FileNotFoundError):
+        raise_for_nonexistance(PARENT_DIR_PATH / "warlpiri.toml")
 
 
 def test_get_project_root() -> None:
