e04f6f63c2d46d82a23b3bc4a02ca5f17f3a150d
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 20:23:35 -0600

    add TODO comments & some error checking

--- translation_library/utils/toml_utils.py
+++ translation_library/utils/toml_utils.py
@@ -3,18 +3,19 @@ from typing import Annotated
 
 import tomlkit
 from glom import glom  # type: ignore
+
+# from glom.core import NonExistentKey  # type: ignore
 from pydantic import BeforeValidator, Field, validate_call
 
-from translation_library.utils.path_utils import (
-    get_language_file_path,
-    valid_path_validator,
-)
+from translation_library.utils.path_utils import valid_path_validator
 
 
+# TODO: validate that toml_file_path ends with .toml
 @validate_call
 def serialize_toml_dict(
     toml_file_path: Annotated[str | Path, BeforeValidator(valid_path_validator)],
 ) -> dict:
+    # TODO: fix docstring comment
     """
     Return a TOML file as a dictionary of key-value pairs from a specified
     directory path.
@@ -25,15 +26,21 @@ def serialize_toml_dict(
     Returns:
         dict: entire TOML file as a dict
     """
-    with open(toml_file_path, "rb") as f:
-        return tomlkit.load(f)
+    try:
+        with open(toml_file_path, "rb") as f:
+            return tomlkit.load(f)
+    except Exception:
+        # TODO: log this
+        raise
 
 
+# TODO: validate that toml_file_path ends with .toml
 @validate_call
 def deserialize_toml_dict(
     toml_data: Annotated[dict, Field(min_length=1)],
     toml_file_path: Annotated[str | Path, BeforeValidator(valid_path_validator)],
 ) -> None:
+    # TODO: fix docstring comment
     """
     Write a TOML-like dictionary to a specified directory path.
 
@@ -41,17 +48,29 @@ def deserialize_toml_dict(
         toml_data (dict): TOML-like dictionary to be deserialized
         toml_file_path (str | Path): path of the TOML file to write to
     """
-    with open(toml_file_path, "w") as f:
-        return tomlkit.dump(toml_data, f)
+    try:
+        with open(toml_file_path, "w") as f:
+            return tomlkit.dump(toml_data, f)
+    except Exception:
+        # TODO: log this
+        raise
 
 
 @validate_call
 def get_value_from_key(
-    language: Annotated[str, Field(min_length=1)],
+    toml_file_path: Annotated[str | Path, BeforeValidator(valid_path_validator)],
     key_path: Annotated[str, Field(min_length=1)],
 ) -> object:
+    # TODO: docstring comment
     try:
-        language_toml_dict: dict = serialize_toml_dict(get_language_file_path(language))
+        language_toml_dict: dict = serialize_toml_dict(toml_file_path)
         return glom(language_toml_dict, key_path)
+    # TODO: find where this exception is
+    # except NonExistentKey as neke:
+    #     raise
+    except FileNotFoundError as fnfe:
+        # TODO: log this
+        raise
     except Exception as e:
-        raise RuntimeError(f"An exception occured: {e}") from e
+        # TODO: log this
+        raise RuntimeError(f"An exception occurred: {e}") from e
