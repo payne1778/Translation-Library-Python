05e6f227dbc1998880e340192c3edf2e6ef594c5
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-07 06:52:31 -0600

    delete unused functions & improve function name clarity

--- translation_library/utils/language_utils.py
+++ translation_library/utils/language_utils.py
@@ -1,26 +1,16 @@
 import logging
-from pathlib import Path
 
 import tomlkit
 from pydantic import Field, validate_call
 
-from translation_library.utils.path_utils import (
+from translation_library.utils.config_utils import (
+    get_all_english_names,
+    get_all_native_names,
     get_language_file_path,
-    get_languages_file_path,
-    get_project_root,
 )
 from translation_library.utils.toml_utils import get_value_from_key, serialize_toml_dict
 
 logger = logging.getLogger(__name__)
-
-
-def local_get_language_file_path(code: str) -> Path:
-    config_path: Path = Path(get_project_root() / "config.toml")
-    i18n_dir: Path = Path(str(get_value_from_key(config_path, "paths.i18n_dir")))
-    language_file_path: Path = Path(
-        str(get_value_from_key(config_path, f"languages.{code.lower()}.file"))
-    )
-    return get_project_root() / i18n_dir / language_file_path
 
 
 @validate_call
@@ -39,24 +29,6 @@ def into_toml_dict(language: str = Field(..., min_length=1)) -> dict:
         logger.info("Successfuly retrieved toml dict from '%s.toml'", language)
         return toml_dict
     logger.warning("None dict retrieved from '%s.toml'", language)
-    return {}
-
-
-def get_languages_toml_dict() -> dict:
-    """
-    Returns a TOML-like dictionary of all of the translation library's supported
-    languages. Example:
-
-    >>> {"english": "English, "german": "Deutsch", "spanish": "Español"}
-
-    Returns:
-        dict: TOML-like dict of supported languages or {} if file was empty
-    """
-    logger.debug("Attempting to retrieve TOML dict from 'languages.toml'")
-    if toml_dict := serialize_toml_dict(get_languages_file_path()):
-        logger.info("Successfuly retrieved toml dict from 'languages.toml'")
-        return toml_dict
-    logger.warning("None dict retrieved from 'languages.toml'")
     return {}
 
 
@@ -93,7 +65,7 @@ def print_toml_dict(language: str = Field(..., min_length=1)) -> None:
     print(into_toml_str(language))
 
 
-def get_languages() -> list[str]:
+def get_languages(lowercase: bool = False) -> list[str]:
     """
     Returns a list of all supported languages according to the languages TOML
     file. Each entry in the list is the language spelled in its native spelling:
@@ -104,22 +76,26 @@ def get_languages() -> list[str]:
         list[str]: list of all supported languages with their native spelling
     """
     logger.debug("Attempting to retrieve list of supported languages")
-    return [language for language in get_languages_toml_dict().values()]
+    if lowercase:
+        return [name.lower() for name in get_all_native_names()]
+    return get_all_native_names()
 
 
-def get_languages_anglicized() -> list[str]:
+def get_languages_as_english_names(lowercase: bool = False) -> list[str]:
     """
     Returns a list of all supported languages according to the languages TOML
-    file. Each entry in the list is the language spelled in its anglicized
+    file. Each entry in the list is the language spelled in its english
     spelling:
 
     >>> ["english", "german", "spanish"]
 
     Returns:
-        list[str]: list of all supported languages with their anglicized spelling
+        list[str]: list of all supported languages with their english spelling
     """
-    logger.debug("Attempting to retrieve list of supported languages anglicized")
-    return [language for language in get_languages_toml_dict()]
+    logger.debug("Attempting to retrieve list of supported languages as english names")
+    if lowercase:
+        return [name.lower() for name in get_all_english_names()]
+    return get_all_english_names()
 
 
 @validate_call
@@ -133,9 +109,9 @@ def is_supported(language: str = Field(..., min_length=1)) -> bool:
     Returns:
         bool: `True` if the language is supported, `False` otherwise
     """
-    supported: bool = language.lower() in [
-        l.lower() for l in get_languages_anglicized()
-    ] or language.lower() in [l.lower() for l in get_languages()]
+    supported: bool = language.lower() in get_languages(
+        lowercase=True
+    ) or language.lower() in get_languages_as_english_names(lowercase=True)
 
     logger.debug("'%s' is supported? '%s'", language, str(supported))
     return supported
