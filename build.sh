e4dee6f44fc122e6427ae1b053e7fb87726d8ff0
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-07 05:35:53 -0600

    update script again to fix another repo import directory bug

--- build.sh
+++ build.sh
@@ -18,13 +18,10 @@ pip install tomlkit > /dev/null
 pip install glom > /dev/null
 pip install pydantic > /dev/null
 
-# 3. Put contents of the docs repo into a new folder named with $DOCS_REPO_NAME
-git subtree add --prefix="$DOCS_REPO_NAME/" "$DOCS_REPO_URL" "$COMMIT" --squash
-
-# 4. If $RESOURCES_DIR doesn't exist, create it before adding files 
+# 3. If $RESOURCES_DIR doesn't exist, create it before adding files 
 if [ ! -d $RESOURCES_DIR ]; then
     mkdir "$RESOURCES_DIR"    
 fi
 
-# 5. Move files from the docs repo stored in $DOCS_REPO_NAME into $RESOURCES_DIR
-mv "$DOCS_REPO_NAME" "$RESOURCES_DIR"
+# 4. Put contents of the docs repo into a new folder named with $DOCS_REPO_NAME
+git subtree add --prefix="$RESOURCES_DIR/" "$DOCS_REPO_URL" "$COMMIT" --squash
