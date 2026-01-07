63977cfd6455227bf9e942019f583baa158bad07
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 22:47:32 -0600

    add more constants to test with

--- tests/utils/constants/values.py
+++ tests/utils/constants/values.py
@@ -1,3 +1,7 @@
+"""
+const values used by unit tests for the `utils` module
+"""
+
 from pathlib import Path
 
 PARENT_DIR_PATH = Path(__file__).resolve().parent
@@ -7,6 +11,10 @@ EXAMPLE_UNSUPPORTED_LANGUAGE = "warlpiri"
 EXAMPLE_SUPPORTED_LANGUAGE = "english"
 
 EXAMPLE_UNSUPPORTED_LANGUAGE_TOML_PATH = Path(f"{EXAMPLE_UNSUPPORTED_LANGUAGE}.toml")
+
+EXAMPLE_UNSUPPORTED_FILE_EXTENSION_PATH = PARENT_DIR_PATH / "example.json"
+
+EXAMPLE_INVALID_TOML_SYNTAX_PATH = PARENT_DIR_PATH / "invalid.toml"
 
 EXAMPLE_ENGLISH_TOML_PATH: Path = PARENT_DIR_PATH / "example.toml"
 
