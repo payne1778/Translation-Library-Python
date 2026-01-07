fc615b07dbd0d157f7203fddbddaa4b78ecb832d
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 19:07:51 -0600

    add arg validation & remove update toml functions

--- translation_library/utils/toml_utils.py
+++ translation_library/utils/toml_utils.py
@@ -1,43 +1,57 @@
-from os.path import exists
 from pathlib import Path
-from typing import Any
+from typing import Annotated
 
-import dpath.util
 import tomlkit
+from glom import glom  # type: ignore
+from pydantic import BeforeValidator, Field, validate_call
+
+from translation_library.utils.path_utils import (
+    get_language_file_path,
+    valid_path_validator,
+)
 
 
-def serialize_toml_dict(toml_file_path: str | Path) -> dict:
-    # """
-    # Return a toml file as a dictionary of key-value pairs from a specified
-    # directory path.
+@validate_call
+def serialize_toml_dict(
+    toml_file_path: Annotated[str | Path, BeforeValidator(valid_path_validator)],
+) -> dict:
+    """
+    Return a TOML file as a dictionary of key-value pairs from a specified
+    directory path.
 
-    # :param toml_file_path: The path to the toml file to be loaded
-    # :return: The entire toml file represented as a dict
-    # """
-    if not exists(toml_file_path):
-        raise ValueError(f"'{toml_file_path}' does not exist")
+    Args:
+        toml_file_path (str | Path): path of the TOML file to be loaded
+
+    Returns:
+        dict: entire TOML file as a dict
+    """
     with open(toml_file_path, "rb") as f:
         return tomlkit.load(f)
 
 
-def deserialize_toml_dict(toml_data: dict, toml_file_path: str | Path) -> None:
-    if not exists(toml_file_path):
-        raise ValueError(f"'{toml_file_path}' does not exist")
+@validate_call
+def deserialize_toml_dict(
+    toml_data: Annotated[dict, Field(min_length=1)],
+    toml_file_path: Annotated[str | Path, BeforeValidator(valid_path_validator)],
+) -> None:
+    """
+    Write a TOML-like dictionary to a specified directory path.
+
+    Args:
+        toml_data (dict): TOML-like dictionary to be deserialized
+        toml_file_path (str | Path): path of the TOML file to write to
+    """
     with open(toml_file_path, "w") as f:
         return tomlkit.dump(toml_data, f)
 
 
-def update_keys(toml_file_path: str | Path, key: str, new_value: Any) -> None:
-    if not exists(toml_file_path):
-        raise ValueError(f"'{toml_file_path}' does not exist")
-    toml_data: dict = serialize_toml_dict(toml_file_path)
-    dpath.util.set(toml_data, f"**/{key}", new_value)
-    deserialize_toml_dict(toml_data, toml_file_path)
-
-
-def update_value(toml_file_path: str | Path, key_path: str, new_value: Any) -> None:
-    if not exists(toml_file_path):
-        raise ValueError(f"'{toml_file_path}' does not exist")
-    toml_data: dict = serialize_toml_dict(toml_file_path)
-    dpath.util.set(toml_data, key_path, new_value)
-    deserialize_toml_dict(toml_data, toml_file_path)
+@validate_call
+def get_value_from_key(
+    language: Annotated[str, Field(min_length=1)],
+    key_path: Annotated[str, Field(min_length=1)],
+) -> object:
+    try:
+        language_toml_dict: dict = serialize_toml_dict(get_language_file_path(language))
+        return glom(language_toml_dict, key_path)
+    except Exception as e:
+        raise RuntimeError(f"An exception occured: {e}") from e
