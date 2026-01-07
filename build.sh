2b3c3c79fdb2b7c1d1eb02fcc55e8b5ab03e730b
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-07 05:33:35 -0600

    update script to fix repo import dir bug & delete docs repo contents for now

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -e 
 
 RESOURCES_DIR="resources"
-I18N_DIR="$RESOURCES_DIR/i18n"
 DOCS_REPO_NAME="Translation-Library-Docs"
 DOCS_REPO_URL="https://github.com/payne1778/$DOCS_REPO_NAME"
 COMMIT="main"
@@ -22,12 +21,10 @@ pip install pydantic > /dev/null
 # 3. Put contents of the docs repo into a new folder named with $DOCS_REPO_NAME
 git subtree add --prefix="$DOCS_REPO_NAME/" "$DOCS_REPO_URL" "$COMMIT" --squash
 
-# 4. If $RESOURCES_DIR or $I18N_DIR don't exist, create them before adding files 
+# 4. If $RESOURCES_DIR doesn't exist, create it before adding files 
 if [ ! -d $RESOURCES_DIR ]; then
     mkdir "$RESOURCES_DIR"    
-elif [ ! -d $I18N_DIR ]; then
-    mkdir "$I18N_DIR"
 fi
 
-# 5. Move files from the docs repo stored in $DOCS_REPO_NAME into $I18N_DIR
-mv "$DOCS_REPO_NAME" "$I18N_DIR"
+# 5. Move files from the docs repo stored in $DOCS_REPO_NAME into $RESOURCES_DIR
+mv "$DOCS_REPO_NAME" "$RESOURCES_DIR"
