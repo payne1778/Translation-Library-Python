cca5f14c6b0f7a094dbba324db03fb8b7e98d09f
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 22:48:23 -0600

    add more test cases for de/serialize_toml_dict()

--- tests/utils/test_toml_utils.py
+++ tests/utils/test_toml_utils.py
@@ -3,6 +3,8 @@ import pytest
 from tests.utils.constants.values import (
     EXAMPLE_ENGLISH_TOML_DICT,
     EXAMPLE_ENGLISH_TOML_PATH,
+    EXAMPLE_INVALID_TOML_SYNTAX_PATH,
+    EXAMPLE_UNSUPPORTED_FILE_EXTENSION_PATH,
     EXAMPLE_UNSUPPORTED_LANGUAGE_TOML_PATH,
 )
 from translation_library.utils.toml_utils import (
@@ -18,9 +20,31 @@ def test_serialize_toml() -> None:
     assert serialize_toml_dict(EXAMPLE_ENGLISH_TOML_PATH) == EXAMPLE_ENGLISH_TOML_DICT
 
 
+def test_serialize_toml_wrong_extension_fail() -> None:
+    with pytest.raises(ValueError):
+        serialize_toml_dict(EXAMPLE_UNSUPPORTED_FILE_EXTENSION_PATH)
+
+
+def test_serialize_toml_invalid_toml_syntax_fail() -> None:
+    with pytest.raises(Exception):
+        serialize_toml_dict(EXAMPLE_INVALID_TOML_SYNTAX_PATH)
+
+
 def test_deserialize_toml() -> None:
     deserialize_toml_dict(EXAMPLE_ENGLISH_TOML_DICT, EXAMPLE_ENGLISH_TOML_PATH)
     assert serialize_toml_dict(EXAMPLE_ENGLISH_TOML_PATH) == EXAMPLE_ENGLISH_TOML_DICT
+
+
+def test_deserialize_toml_wrong_extension_fail() -> None:
+    with pytest.raises(ValueError):
+        deserialize_toml_dict(
+            EXAMPLE_ENGLISH_TOML_DICT, EXAMPLE_UNSUPPORTED_FILE_EXTENSION_PATH
+        )
+
+
+def test_deserialize_toml_empty_dict_fail() -> None:
+    with pytest.raises(Exception):
+        deserialize_toml_dict({}, EXAMPLE_ENGLISH_TOML_PATH)
 
 
 def test_get_value_from_key_pass() -> None:
