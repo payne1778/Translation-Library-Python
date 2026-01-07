b1c4dda141d9c9b98caf7fed10bd24f92aad6294
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-07 06:50:40 -0600

    update language utils tests to test more recent functions

--- tests/utils/test_language_utils.py
+++ tests/utils/test_language_utils.py
@@ -4,9 +4,10 @@ from tests.utils.constants.values import (
 )
 from translation_library.utils.language_utils import (
     get_languages,
-    get_languages_anglicized,
-    get_languages_toml_dict,
+    get_languages_as_english_names,
+    get_value_from_language_toml,
     into_toml_dict,
+    into_toml_str,
     is_supported,
 )
 
@@ -15,16 +16,22 @@ def test_language_toml() -> None:
     assert isinstance(into_toml_dict(EXAMPLE_SUPPORTED_LANGUAGE), dict)
 
 
-def test_languages_toml() -> None:
-    assert isinstance(get_languages_toml_dict(), dict)
-
-
 def test_get_languages() -> None:
     assert EXAMPLE_SUPPORTED_LANGUAGE in get_languages()
 
 
-def test_get_languages_anglicized() -> None:
-    assert EXAMPLE_SUPPORTED_LANGUAGE in get_languages_anglicized()
+def test_get_languages_fail() -> None:
+    assert EXAMPLE_UNSUPPORTED_LANGUAGE not in get_languages()
+
+
+def test_get_languages_as_english_names() -> None:
+    assert EXAMPLE_SUPPORTED_LANGUAGE in get_languages_as_english_names(lowercase=True)
+
+
+def test_get_languages_as_english_names_fail() -> None:
+    assert EXAMPLE_UNSUPPORTED_LANGUAGE not in get_languages_as_english_names(
+        lowercase=True
+    )
 
 
 def test_is_supported_language() -> None:
@@ -33,3 +40,21 @@ def test_is_supported_language() -> None:
 
 def test_is_supported_language_fail() -> None:
     assert not is_supported(EXAMPLE_UNSUPPORTED_LANGUAGE)
+
+
+def test_into_toml_str() -> None:
+    assert isinstance(into_toml_str(EXAMPLE_SUPPORTED_LANGUAGE), str)
+
+
+# def test_get_value_from_language_toml() -> None:
+#     assert (
+#         get_value_from_language_toml(EXAMPLE_SUPPORTED_LANGUAGE, "hello")
+#         == "Hello {name}"
+#     )
+
+
+# def test_get_value_from_language_toml() -> None:
+#     assert (
+#         get_value_from_language_toml(EXAMPLE_SUPPORTED_LANGUAGE, "hello")
+#         == "Hello {name}"
+#     )
