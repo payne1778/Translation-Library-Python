97ac9f6354fefaff029ca26c38a47c6c850ff974
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-07 05:23:20 -0600

    update script to install docs repo contents into a specific dir

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -e 
 
-LIB_DIR="lib"
+RESOURCES_DIR="resources"
+I18N_DIR="$RESOURCES_DIR/i18n"
 DOCS_REPO_NAME="Translation-Library-Docs"
 DOCS_REPO_URL="https://github.com/payne1778/$DOCS_REPO_NAME"
 COMMIT="main"
@@ -11,18 +12,22 @@ COMMIT="main"
 python3 -m venv .venv
 source .venv/bin/activate
 
+# TODO: move dependencies installation to future pyproject.toml
 # 2. Install dependencies
 pip install pytest > /dev/null
 pip install tomlkit > /dev/null
-pip install dpath > /dev/null
+pip install glom > /dev/null
+pip install pydantic > /dev/null
 
 # 3. Put contents of the docs repo into a new folder named with $DOCS_REPO_NAME
 git subtree add --prefix="$DOCS_REPO_NAME/" "$DOCS_REPO_URL" "$COMMIT" --squash
 
-# 4. If the $LIB_DIR doesn't exist, create it before adding files to it
-if [ ! -d $LIB_DIR ]; then
-    mkdir "$LIB_DIR"    
+# 4. If $RESOURCES_DIR or $I18N_DIR don't exist, create them before adding files 
+if [ ! -d $RESOURCES_DIR ]; then
+    mkdir "$RESOURCES_DIR"    
+elif [ ! -d $I18N_DIR ]; then
+    mkdir "$I18N_DIR"
 fi
 
-# 5. Move files from the docs repo stored in $DOCS_REPO_NAME into $LIB_DIR
-mv "$DOCS_REPO_NAME" "$LIB_DIR"
+# 5. Move files from the docs repo stored in $DOCS_REPO_NAME into $I18N_DIR
+mv "$DOCS_REPO_NAME" "$I18N_DIR"
