6b9cc03d59318212b70a9b6d1461419227c75f44
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-06 19:06:42 -0600

    abstract hardcoded strings as constants & add tests for valid_path_validator()

--- tests/utils/test_paths_utils.py
+++ tests/utils/test_paths_utils.py
@@ -6,34 +6,53 @@ from translation_library.utils.path_utils import (
     get_language_file_path,
     get_languages_file_path,
     get_project_root,
-    raise_for_nonexistance,
+    valid_path_validator,
 )
 
 PARENT_DIR_PATH = Path(__file__).resolve().parent
 
 EXAMPLE_ENGLISH_TOML_PATH: Path = PARENT_DIR_PATH / "example_english.toml"
 
+EXAMPLE_UNSUPPORTED_LANGUAGE = "warlpiri"
 
-def test_raise_for_nonexistance() -> None:
-    raise_for_nonexistance(EXAMPLE_ENGLISH_TOML_PATH)
+EXAMPLE_SUPPORTED_LANGUAGE = "english"
 
 
-def test_raise_for_nonexistance_fail() -> None:
+def test_valid_path_validator() -> None:
+    valid_path_validator(EXAMPLE_ENGLISH_TOML_PATH)
+
+
+def test_valid_path_validator_empty_str_fail() -> None:
+    with pytest.raises(ValueError):
+        valid_path_validator("")
+
+
+def test_valid_path_validator_nonexistant_str_path_fail() -> None:
     with pytest.raises(FileNotFoundError):
-        raise_for_nonexistance(PARENT_DIR_PATH / "warlpiri.toml")
+        valid_path_validator(f"{EXAMPLE_UNSUPPORTED_LANGUAGE}.toml")
+
+
+def test_valid_path_validator_nonexistant_path_fail() -> None:
+    with pytest.raises(FileNotFoundError):
+        valid_path_validator(Path(f"{EXAMPLE_UNSUPPORTED_LANGUAGE}.toml"))
 
 
 def test_get_project_root() -> None:
     assert get_project_root().exists()
 
 
+def test_get_project_root_fail() -> None:
+    with pytest.raises(FileNotFoundError):
+        get_project_root(anchor=f"{EXAMPLE_UNSUPPORTED_LANGUAGE}.toml")
+
+
 def test_get_language_file_path() -> None:
-    assert get_language_file_path("english").exists()
+    assert get_language_file_path(EXAMPLE_SUPPORTED_LANGUAGE).exists()
 
 
 def test_get_language_file_path_fail() -> None:
     with pytest.raises(FileNotFoundError):
-        get_language_file_path("warlpiri")
+        get_language_file_path(EXAMPLE_UNSUPPORTED_LANGUAGE)
 
 
 def test_get_languages_file_path() -> None:
