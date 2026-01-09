8e694a87a1db14961f0def5f3992bcb351c82f6f
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-09 07:30:38 -0500

    ignore logs dir and add future logs to logs dir

--- translation_library/__init__.py
+++ translation_library/__init__.py
@@ -8,5 +8,5 @@ import logging
 logging.basicConfig(
     level=logging.DEBUG,
     format="[%(levelname)s] (%(asctime)s) %(funcName)s(): %(message)s ['%(pathname)s:%(lineno)s']",
-    filename=f"{datetime.datetime.now().strftime("%H:%M:%S_%m-%d-%y")}.log",
+    filename=f"logs/{datetime.datetime.now().strftime("%H:%M:%S_%m-%d-%y")}.log",
 )
