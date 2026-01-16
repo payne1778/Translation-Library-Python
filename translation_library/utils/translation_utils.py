b537cc9699ed841a193893253feedb67316f28a2
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-16 09:14:53 -0500

    change bool param from lowercase to casefold

--- translation_library/utils/translation_utils.py
+++ translation_library/utils/translation_utils.py
@@ -4,6 +4,7 @@ from pydantic import Field, validate_call
 
 from translation_library.utils.config_utils import (
     get_all_english_names,
+    get_all_language_codes,
     get_all_native_names,
     get_language_file_path,
 )
@@ -12,23 +13,26 @@ from translation_library.utils.toml_utils import get_value_from_key
 logger = logging.getLogger(__name__)
 
 
-def get_languages(lowercase: bool = False) -> list[str]:
+def get_languages(casefold: bool = False) -> list[str]:
     """
     Returns a list of all supported languages according to the languages TOML
     file. Each entry in the list is the language spelled in its native spelling:
 
     >>> ["English", "Deutsch", "Español"]
 
+    Args:
+        language (str): TODO: fixme!!
+
     Returns:
         list[str]: list of all supported languages with their native spelling
     """
     logger.debug("Attempting to retrieve list of supported languages")
-    if lowercase:
-        return [name.lower() for name in get_all_native_names()]
+    if casefold:
+        return [name.casefold() for name in get_all_native_names()]
     return get_all_native_names()
 
 
-def get_languages_as_english_names(lowercase: bool = False) -> list[str]:
+def get_languages_as_english_names(casefold: bool = False) -> list[str]:
     """
     Returns a list of all supported languages according to the languages TOML
     file. Each entry in the list is the language spelled in its english
@@ -36,15 +40,19 @@ def get_languages_as_english_names(lowercase: bool = False) -> list[str]:
 
     >>> ["english", "german", "spanish"]
 
+    Args:
+        casefold (bool): TODO: fixme!!
+
     Returns:
         list[str]: list of all supported languages with their english spelling
     """
     logger.debug("Attempting to retrieve list of supported languages as english names")
-    if lowercase:
-        return [name.lower() for name in get_all_english_names()]
+    if casefold:
+        return [name.casefold() for name in get_all_english_names()]
     return get_all_english_names()
 
 
+# TODO: make `language` into `language_code`
 @validate_call
 def is_supported(language: str = Field(..., min_length=1)) -> bool:
     """
@@ -56,14 +64,12 @@ def is_supported(language: str = Field(..., min_length=1)) -> bool:
     Returns:
         bool: `True` if the language is supported, `False` otherwise
     """
-    supported: bool = language.lower() in get_languages(
-        lowercase=True
-    ) or language.lower() in get_languages_as_english_names(lowercase=True)
-
+    supported: bool = language in get_all_language_codes()
     logger.debug("'%s' is supported? '%s'", language, str(supported))
     return supported
 
 
+# TODO: make `language` into `language_code`
 @validate_call
 def get_i18n_obj(
     language: str = Field(..., min_length=1), key_path: str = Field(..., min_length=1)
